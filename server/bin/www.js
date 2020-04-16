e62f69874963cb9bfe5254697688c9490c38cd3f
Peter Joles <jolzee@gmail.com>
2020-04-15 17:37:42 -0700

    Changed default port to 5000

--- server/bin/www.js
+++ server/bin/www.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use("/", router);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 router.post("/", handleInboundSms);
 
